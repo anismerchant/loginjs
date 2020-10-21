0d06504d55cc054dfe8ae6a5d8110ff031fd3c30
Anis Merchant <anisxmerchant@gmail.com>
2020-10-21 13:52:28 -0400

    chore: setup test routes

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const helmet = require('helmet');
 const connectDB = require('./config/db');
+const users = require('./routes/api/users');
+const auth = require('./routes/api/auth');
 
 // protects against well-known vulnerabilities
 // and sets http headers up-front
@@ -10,9 +12,18 @@ app.use(helmet());
 // connect database
 connectDB();
 
+// @route  GET /
+// @desc   Test route
+// @access Public
+app.get('/', (req, res) => res.send('API Running'));
+
 // init middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// define routes
+app.use('/api/users', users);
+app.use('/api/auth', auth);
 
 const PORT = process.env.PORT || 5000;
 
