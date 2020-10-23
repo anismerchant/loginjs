45b11af44304c78ce86b0a3f12593fbae2119d73
Anis Merchant <anisxmerchant@gmail.com>
2020-10-23 17:43:29 -0400

    refactor: validate optional parameter type

--- server.js
+++ server.js
@@ -15,7 +15,11 @@ const loginUser = require('./routes/api/login');
 
 exports = module.exports = createLogin;
 
-function createLogin(mongodbURI, jwtSecret, register, login) {
+// de-structure loginConfig
+function createLogin({
+	required: { mongodbURI, jwtSecret, registerRoute, loginRoute },
+	optional: { passwordLength = 8, jwtExpiration = 7200 },
+}) {
 	// protects against well-known vulnerabilities
 	// and sets http headers up-front
 	app.use(helmet());
@@ -27,9 +31,24 @@ function createLogin(mongodbURI, jwtSecret, register, login) {
 	app.use(express.urlencoded({ extended: true }));
 	app.use(express.json());
 
-	// define routes
-	app.use(register, registerUser(jwtSecret));
-	app.use(login, loginUser(jwtSecret));
+	// validate optional parameter type
+	try {
+		if (!Number.isInteger(passwordLength) || !Number.isInteger(jwtExpiration)) {
+			return console.log(
+				'"passwordLength" and "jwtExpiration" must be integers.'
+			);
+		}
+
+		// define routes
+		app.use(
+			registerRoute,
+			registerUser(jwtSecret, passwordLength, jwtExpiration)
+		);
+		app.use(loginRoute, loginUser(jwtSecret, jwtExpiration));
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
 
 	const PORT = process.env.PORT || 5000;
 
