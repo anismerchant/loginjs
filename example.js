004a761285e4494f54741d228d20530a3bb556f0
Anis Merchant <anisxmerchant@gmail.com>
2020-12-16 03:42:38 -0500

    chore: update example

--- example.js
+++ example.js
@@ -1,3 +1,6 @@
+const express = require('express');
+const app = express();
+const helmet = require('helmet');
 const loginJS = require('login-express');
 
 // required
@@ -22,20 +25,34 @@ const appConfig = {
 // appConfig.jwtResetExpiration = parseInt(process.env.JWT_RESET_EXPIRATION); // in seconds
 
 // Optional
-let verifyEmailConfig = {
-	emailHeading: 'Your Company Name',
-	emailSubjectLine: 'Verify Password',
-	emailMessage:
-		'Custom verify password message goes here. Verify link auto-generated.',
-};
+// let verifyEmailConfig = {
+// 	emailHeading: 'Your Company Name',
+// 	emailSubjectLine: 'Verify Password',
+// 	emailMessage:
+// 		'Custom verify password message goes here. Verify link auto-generated.',
+// };
 
 // Optional
-let resetEmailConfig = {
-	emailHeading: 'Your Company Name',
-	emailSubjectLine: 'Reset Password',
-	emailMessage:
-		'Custom reset password message goes here. Reset link auto-generated.',
-};
+// let resetEmailConfig = {
+// 	emailHeading: 'Your Company Name',
+// 	emailSubjectLine: 'Reset Password',
+// 	emailMessage:
+// 		'Custom reset password message goes here. Reset link auto-generated.',
+// };
 
+app.use(helmet());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-loginJS(dbConfig, appConfig, verifyEmailConfig = {}, resetEmailConfig = {});
+loginJS(
+	dbConfig,
+	appConfig,
+	(verifyEmailConfig = {}),
+	(resetEmailConfig = {}),
+	app,
+	express
+);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
